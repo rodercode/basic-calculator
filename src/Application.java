2b77a0d7665c47a12ac1ac4d2ab2075e02258595
Rolandsson <102300415+Rolandsson@users.noreply.github.com>
2022-11-21 13:06:31 +0100

    Add files via upload

---
+++ src/Application.java
@@ -0,0 +1,10 @@
+public class Application {
+
+  public static void main(String args) {
+    // Skapa en enklare miniräknare som kan addera, subtrahera och multiplicera två tal
+    // ex.  add(2, 5) // 10
+    //      sub(2, 5) // -3
+
+    // Level up, gör så att varje operation kan utföra operationen på ett godtyckligt antal tal
+  }
+}
