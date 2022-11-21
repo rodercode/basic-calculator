814bffd213b7738541bf6c76241224d41dd9a7c7
maxr <maxr@ubuntu-desktop.myguest.virtualbox.org>
2022-11-21 14:18:46 +0100

    Created an addition branch

---
+++ addition.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Addition {
+
+	public void calculate(){
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+
+		int x = a + b;
+
+		System.out.println(x);					}
+           }
+}
